7877604f5abee11d058d745ede8bad5996adc5fe
jiandongliu <jiandong.liu@forvision-tech.com>
2024-06-06 16:46:29 +0800

    "添加预编译阶段的代码"

---
+++ basic/01_low_level_principles/preprocessing/avs.i
@@ -0,0 +1,31 @@
+# 1 "avs.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "avs.c"
+# 17 "avs.c"
+# 1 "avs.h" 1
+# 17 "avs.h"
+       
+# 1 "avs_once.h" 1
+# 18 "avs.h" 2
+# 18 "avs.c" 2
+
+
+
+
+
+#pragma INVALID_DEFINE
+int main()
+{
+
+    int val = 0;
+    val += (2 3 4);
+    while (1)
+    {
+    }
+
+    return 0;
+}
---
+++ basic/01_low_level_principles/preprocessing/avs_once.h
@@ -0,0 +1,17 @@
+/**
+ * @file 01_vas.cpp
+ * @brief C++基础课程\01 深入学习C++语言先要练好的内功\掌握进程虚拟地址空间区域划分_ev
+ * @author jiandong.liu (jiandong_liu@outlook.com)
+ * @version 0.1
+ * @date 2024-06-04
+ *
+ * @copyright Copyright (c) 2024 JiandongLiu
+ *
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date       <th>Version  <th>Author       <th>Description
+ * <tr><td>2024-06-04 <td>1.0      <td>jiandong.liu     <td>first version
+ * </table>
+ */
+// #include <stdio.h>
+#include "avs.h"
