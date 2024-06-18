1466776b3940aeb44be71b974f84cd1a6ac0cc9a
jiandongliu <jiandong.liu@forvision-tech.com>
2024-06-18 14:46:42 +0800

    "装载相关的代码"

---
+++ basic/01_low_level_principles/loading/main.c
@@ -0,0 +1,24 @@
+/**
+ * @file main.c
+ * @brief
+ * @author jiandong.liu (jiandong_liu@outlook.com)
+ * @version 0.1
+ * @date 2024-06-17
+ *
+ * @copyright Copyright (c) 2024 JiandongLiu
+ *
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date       <th>Version  <th>Author       <th>Description
+ * <tr><td>2024-06-17 <td>1.0      <td>jiandong.liu     <td>first version
+ * </table>
+ */
+#include <stdio.h>
+
+int main()
+{
+    while (1)
+    {
+        sleep(1000);
+    }
+}
