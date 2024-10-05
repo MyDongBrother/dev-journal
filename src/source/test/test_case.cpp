4d7a5973065822b223b1a63a9c1d4a52d1edfa5a
jiandong <jiandong_liu@outlook.com>
2024-10-05 18:05:43 +0800

    "#pragma pack(n)章节"

--- src/source/test/test_case.cpp
+++ src/source/test/test_case.cpp
@@ -1,5 +1,29 @@
 #include "test_case.h"
+#pragma pack(push) // 保存当前的对齐状态
+#pragma pack(1)    // 设置为 4 字节对齐
 
+struct test_1byte
+{
+    char m1;   // 1 字节
+    double m4; // 8 字节（可能会有填充）
+    int m3;    // 4 字节
+};
+#pragma pack(4) // 设置为 4 字节对齐
+
+struct test_4byte
+{
+    char m1;   // 4 字节
+    double m4; // 8 字节（可能会有填充）
+    int m3;    // 4 字节
+};
+
+#pragma pack(pop) // 恢复之前的对齐状态
+struct test_default
+{
+    char m1;   // 8 字节
+    double m4; // 8 字节（可能会有填充）
+    int m3;    // 8 字节
+};
 namespace uss_source
 {
 
@@ -10,12 +34,10 @@ TestCase::TestCase(std::shared_ptr<runcontex::RunContex> f_runcontex)
 void TestCase::Init() {}
 void TestCase::Run()
 {
-    int arr[10];
-    int *ptr = arr;
-
-    std::cout << "Size of arr: " << sizeof(arr) << " bytes" << std::endl;
-    std::cout << "Size of arr[10]: " << sizeof(arr[10]) << " bytes" << std::endl;
-    std::cout << "Size of pointer: " << sizeof(ptr) << " bytes" << std::endl;
+    std::cout << "Size of test_1byte: " << sizeof(test_1byte) << " bytes" << std::endl;
+    std::cout << "Size of test_4byte: " << sizeof(test_4byte) << " bytes" << std::endl;
+    std::cout << "Size of test_default: " << sizeof(test_default) << " bytes"
+              << std::endl;
     exit(1);
 }
 } // namespace uss_source
