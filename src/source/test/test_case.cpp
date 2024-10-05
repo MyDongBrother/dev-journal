1ecbb17b0dcbf3b2c30ee6bc45e61cf8184319ad
jiandong <jiandong_liu@outlook.com>
2024-10-05 18:09:30 +0800

    "位域"

--- src/source/test/test_case.cpp
+++ src/source/test/test_case.cpp
@@ -1,28 +1,9 @@
 #include "test_case.h"
-#pragma pack(push) // 保存当前的对齐状态
-#pragma pack(1)    // 设置为 4 字节对齐
-
-struct test_1byte
-{
-    char m1;   // 1 字节
-    double m4; // 8 字节（可能会有填充）
-    int m3;    // 4 字节
-};
-#pragma pack(4) // 设置为 4 字节对齐
-
-struct test_4byte
-{
-    char m1;   // 4 字节
-    double m4; // 8 字节（可能会有填充）
-    int m3;    // 4 字节
-};
-
-#pragma pack(pop) // 恢复之前的对齐状态
 struct test_default
 {
-    char m1;   // 8 字节
-    double m4; // 8 字节（可能会有填充）
-    int m3;    // 8 字节
+    unsigned int mode : 2;   // mode 占 2 位
+    unsigned int status : 1; // status 占 1 位
+    unsigned int error : 3;  // error 占 3 位
 };
 namespace uss_source
 {
@@ -34,8 +15,14 @@ TestCase::TestCase(std::shared_ptr<runcontex::RunContex> f_runcontex)
 void TestCase::Init() {}
 void TestCase::Run()
 {
-    std::cout << "Size of test_1byte: " << sizeof(test_1byte) << " bytes" << std::endl;
-    std::cout << "Size of test_4byte: " << sizeof(test_4byte) << " bytes" << std::endl;
+    test_default flags;
+    flags.mode   = 4; // mode 占 2 位，因此最大值为 3（二进制11）
+    flags.status = 2; // status 占 1 位，值为 1
+    flags.error  = 9; // error 占 3 位，最大值为 7
+
+    std::cout << "Mode: " << flags.mode << std::endl;
+    std::cout << "Status: " << flags.status << std::endl;
+    std::cout << "Error: " << flags.error << std::endl;
     std::cout << "Size of test_default: " << sizeof(test_default) << " bytes"
               << std::endl;
     exit(1);
