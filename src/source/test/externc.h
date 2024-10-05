8e760c633192302e414bd5ff14ccba0b85ea5d05
jiandong <jiandong_liu@outlook.com>
2024-10-05 18:22:10 +0800

    "extern c"

---
+++ src/source/test/externc.h
@@ -0,0 +1,26 @@
+/**
+ * @file externc.h
+ * @brief
+ * @author jiandong.liu (jiandong.liu@outlook.com)
+ * @version 0.1
+ * @date 2024-10-05
+ *
+ * @copyright Copyright (c) 2024 jiandong.liu
+ *
+ * @par Change Log:
+ * <table>
+ * <tr><th>Date       <th>Version  <th>Author       <th>Description
+ * <tr><td>2024-10-05 <td>1.0      <td>jiandong.liu     <td>first version
+ * </table>
+ */
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    void my_c_function(int x);
+
+#ifdef __cplusplus
+}
+#endif
--- src/source/test/test_case.cpp
+++ src/source/test/test_case.cpp
@@ -1,10 +1,6 @@
 #include "test_case.h"
-struct test_default
-{
-    unsigned int mode : 2;   // mode 占 2 位
-    unsigned int status : 1; // status 占 1 位
-    unsigned int error : 3;  // error 占 3 位
-};
+#include "externc.h"
+
 namespace uss_source
 {
 
@@ -13,18 +9,5 @@ TestCase::TestCase(std::shared_ptr<runcontex::RunContex> f_runcontex)
 {
 }
 void TestCase::Init() {}
-void TestCase::Run()
-{
-    test_default flags;
-    flags.mode   = 4; // mode 占 2 位，因此最大值为 3（二进制11）
-    flags.status = 2; // status 占 1 位，值为 1
-    flags.error  = 9; // error 占 3 位，最大值为 7
-
-    std::cout << "Mode: " << flags.mode << std::endl;
-    std::cout << "Status: " << flags.status << std::endl;
-    std::cout << "Error: " << flags.error << std::endl;
-    std::cout << "Size of test_default: " << sizeof(test_default) << " bytes"
-              << std::endl;
-    exit(1);
-}
+void TestCase::Run() { my_c_function(12); }
 } // namespace uss_source
